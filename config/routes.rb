33ebbb78011fbdd69b99eab43371779f40cffdc7
michaellarocca90 <33819929+michaellarocca90@users.noreply.github.com>
2019-01-31 07:48:43 -0800

    Authentication and Authorization

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   resources :skills
   resources :profiles
   resources :users
+
+  post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
